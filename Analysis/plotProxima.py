fixed a sign error on the prior

Analysis/plotProxima.py
@@ -9,9 +9,7 @@ import emcee
 import matplotlib.pyplot as plt
 
 
-#filename = "../Data/Rup147.h5"
-filename = "../Data/apRun18.h5"
-#filename = "../Data/Rup147LargeNWalk.h5"
+filename = "../Data/proxima.h5"
 
 # Open file
 reader = emcee.backends.HDFBackend(filename)
@@ -19,7 +17,7 @@ reader = emcee.backends.HDFBackend(filename)
 print(reader.iteration)
 tau = reader.get_autocorr_time(tol=0)
 if np.any(~np.isfinite(tau)):
-    tau = 2500
+    tau = 500
 
 burnin = int(2*np.max(tau))
 thin = int(0.5*np.min(tau))
@@ -28,21 +26,18 @@ chain = reader.get_chain(discard=burnin, flat=True, thin=thin)
 print(burnin)
 
 ### Corner plot ###
-
-labels = ["m1", "m2", "Prot1I", "Prot2I", "Tau1", "Tau2", "PorbI", "EccI",
-          "Age"]
+labels = ["Mass", "SatXUVFrac", "SatXUVTime", "Age", "XUVBeta"]
 #range = [(1.0,1.1),(1.0,1.1),(0,15),(0,15),(-2,3),(-2,3),(3,13),
 #         (0,0.6),(1.0e9,4.0e9)]
 range = None
 
 # MLE solution
-mle = np.array([1.08, 1.07, 7.28, 7.47, -0.17, -0.39, 7.27, 0.28, 2.49])
 
 fig = corner.corner(chain, labels=labels, range=range,
-                    quantiles=[0.16, 0.5, 0.84], truths=mle,
+                    quantiles=[0.16, 0.5, 0.84],
                     show_titles=True, title_kwargs={"fontsize": 12})
 
-fig.savefig("rup147Corner.png", bbox_inches="tight")
+fig.savefig("proximaCorner.png", bbox_inches="tight")
 
 ### Chains
 
@@ -59,4 +54,4 @@ for ii, ax in enumerate(axes.flatten()):
 axes[-1].set_xlabel("Iterations")
 
 fig.tight_layout()
-fig.savefig("rup147Chains.png")
+fig.savefig("proximaChains.png")

ehi/proxima.py
@@ -52,7 +52,7 @@ def LnPriorPROXIMA(x, **kwargs):
         return -np.inf
 
     # log flat prior on saturation timescale log10[Gyr]
-    if (-dSatXUVTime < -0.3) or (-dSatXUVTime > 1.0):
+    if (dSatXUVTime < -0.3) or (dSatXUVTime > 1.0):
         return -np.inf
 
     # Large bound for age of system [Gyr]